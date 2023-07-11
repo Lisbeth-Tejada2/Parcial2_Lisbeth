f83f7f6dae5f74c4edf4dd9fa03e37c6c317b55d
Latitude E7450 <Latitude E7450@LISBETH>
2023-07-11 00:55:50 -0400

    Entrando los modelos en una carpeta para mejor organización (Anteriormente estaban sueltos)

---
+++ Shared/Models/Productos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_Lisbeth.Shared.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public float Tipo { get; set; }
+        public int Existencia { get; set; }
+    }
+}
