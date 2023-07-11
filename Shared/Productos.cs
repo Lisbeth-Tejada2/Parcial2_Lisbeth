d5b2a08397686e55b77a57f84afbfdae6df854dc
Latitude E7450 <Latitude E7450@LISBETH>
2023-07-10 20:11:37 -0400

    Modelo 'Productos' Terminado

--- Shared/Productos.cs
+++ Shared/Productos.cs
@@ -9,6 +9,10 @@ namespace Parcial2_Lisbeth.Shared
 {
     public class Productos
     {
-        
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public float Tipo { get; set; }
+        public int Existencia { get; set; }
     }
 }
