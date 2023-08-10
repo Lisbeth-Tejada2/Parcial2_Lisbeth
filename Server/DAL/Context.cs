ed3ef47acc3c9e891648c3626cbcb615f6cf21e4
Latitude E7450 <Latitude E7450@LISBETH>
2023-08-10 08:43:57 -0400

    Tratando de solucionar excepción

--- Server/DAL/Context.cs
+++ Server/DAL/Context.cs
@@ -19,70 +19,63 @@ namespace Parcial2_Lisbeth.Server.DAL
                 {
                     ProductoId = 1,
                     Descripcion = "Mani",
-                    Tipo = 0,
-                    Existencia = 250,
-                    PrecioDeCompra = 5,
-                    PrecioDeVenta = 8
+                    PrecioDeCompra = 8,
+                    PrecioDeVenta = 15,
+                    Existencia = 250
                 },
 
                 new Productos
                 {
                     ProductoId = 2,
                     Descripcion = "Pistachos",
-                    Tipo = 0,
-                    Existencia = 300,
                     PrecioDeCompra = 15,
-                    PrecioDeVenta = 18
+                    PrecioDeVenta = 30,
+                    Existencia = 300
                 },
 
                 new Productos
                 {
                     ProductoId = 3,
                     Descripcion = "Pasas",
-                    Tipo = 0,
-                    Existencia = 130,
-                    PrecioDeCompra = 5,
-                    PrecioDeVenta = 8
+                    PrecioDeCompra = 10,
+                    PrecioDeVenta = 25,
+                    Existencia = 130
                 },
 
                 new Productos
                 {
                     ProductoId = 4,
                     Descripcion = "Ciruelas",
-                    Tipo = 0,
-                    Existencia = 350,
-                    PrecioDeCompra = 10,
-                    PrecioDeVenta = 15
+                    PrecioDeCompra = 25,
+                    PrecioDeVenta = 50,
+                    Existencia = 350
                 },
 
                 new Productos
                 {
                     ProductoId = 5,
-                    Descripcion = "Mixto MPP 0.5Lb",
-                    Tipo = 1,
-                    Existencia = 320,
+                    Descripcion = "Mixto MPP",
                     PrecioDeCompra = 30,
-                    PrecioDeVenta = 35
+                    PrecioDeVenta = 60,
+                    Existencia = 320
                 },
 
                 new Productos
                 {
                     ProductoId = 6,
-                    Descripcion = "Mixto MPC 0.5Lb",
-                    Tipo = 1,
-                    Existencia = 310,
-                    PrecioDeCompra = 45,
-                    PrecioDeVenta = 50
+                    Descripcion = "Mixto MPC",
+                    PrecioDeCompra = 30,
+                    PrecioDeVenta = 60,
+                    Existencia = 310
                 },
 
                 new Productos
                 {
                     ProductoId = 7,
-                    Descripcion = "Mixto MPP 0.2Lb",
-                    Tipo = 1,
-                    Existencia = 250,
-                    PrecioDeCompra = 20,
-                    PrecioDeVenta = 29
+                    Descripcion = "Mixto MPP",
+                    PrecioDeCompra = 25,
+                    PrecioDeVenta = 50,
+                    Existencia = 250
                 }
             );
         }
